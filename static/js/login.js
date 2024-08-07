184f86ca0e5ac90d5fa7d890cf9a6c9159d35179
desolateMesh <jrochau@elevatedcloudworks.com>
2024-08-07 09:49:28 -0600

    updates

---
+++ static/js/login.js
@@ -0,0 +1,5 @@
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Here you would typically send the login data to your server
+    alert('Login button clicked!');
+});
